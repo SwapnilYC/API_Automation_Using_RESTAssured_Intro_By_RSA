a284da08faf4416d5caf15663d54344d0038b0cd
Swapnil Chaudhari <swap95nil@gmail.com>
2026-04-18 21:31:24 +0530

    Just done formatting. No code change

--- src/test/java/API_Testing/API_Automation_Testing_Using_RESTAssured/BasicsOfRESTAssured/Automating_POST_Request.java
+++ src/test/java/API_Testing/API_Automation_Testing_Using_RESTAssured/BasicsOfRESTAssured/Automating_POST_Request.java
@@ -7,31 +7,22 @@ import static org.hamcrest.Matchers.equalTo;
 
 import API_Testing.API_Automation_Testing_Using_RESTAssured.payLoads.PayLoad;
 import io.restassured.RestAssured;
+
 public class Automating_POST_Request {
 
 	public static void main(String[] args) {
 		// Storing base url
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		
-		// GIVEN
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(PayLoad.createPlace())
-		// WHEN
-		.when().post("maps/api/place/add/json")  
-		// THEN
-		.then().log().all()
-		// Apply assertion on status code
-		.assertThat().statusCode(200)
-		// Applying validations on response body & Header
-		.body("scope", equalTo("APP"))
-		.header("Server", "Apache/2.4.52 (Ubuntu)");         
+
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body(PayLoad.createPlace()).when().post("maps/api/place/add/json").then().log().all().assertThat()
+				.statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
 	}
 
 }
 
 /*
- RESTAssured is based on 3 methods
- 	1. given -> all input data goes here (queryParam -> header -> body)
- 	2. when -> API is hit & resources & HTTP methods goes here
- 	3. then -> response validation goes here 
+ * RESTAssured is based on 3 methods 1. given -> all input data goes here
+ * (queryParam -> header -> body) 2. when -> API is hit & resources & HTTP
+ * methods goes here 3. then -> response validation goes here
  */
- 
