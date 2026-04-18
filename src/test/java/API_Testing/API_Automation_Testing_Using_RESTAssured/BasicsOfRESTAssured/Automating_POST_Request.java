98541ccc4e9d28eb20fc82d0ef22f7a3b429b406
Swapnil Chaudhari <swap95nil@gmail.com>
2026-04-18 19:37:03 +0530

    we moved the payload into another file & import it so the code looks clean. Also if we want to change the pay load we can change it in one file & it will be reflected where it is imported

--- src/test/java/API_Testing/API_Automation_Testing_Using_RESTAssured/BasicsOfRESTAssured/Automating_POST_Request.java
+++ src/test/java/API_Testing/API_Automation_Testing_Using_RESTAssured/BasicsOfRESTAssured/Automating_POST_Request.java
@@ -2,7 +2,10 @@ package API_Testing.API_Automation_Testing_Using_RESTAssured.BasicsOfRESTAssured
 
 // for given, when & then methods. Because these are static methods
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;    // for reponse matcher for body
+// for reponse matcher for body
+import static org.hamcrest.Matchers.equalTo;
+
+import API_Testing.API_Automation_Testing_Using_RESTAssured.payLoads.PayLoad;
 import io.restassured.RestAssured;
 public class Automating_POST_Request {
 
@@ -11,22 +14,7 @@ public class Automating_POST_Request {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		
 		// GIVEN
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body("{\r\n"  
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Aditya Niwas\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}")
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(PayLoad.createPlace())
 		// WHEN
 		.when().post("maps/api/place/add/json")  
 		// THEN
@@ -36,8 +24,6 @@ public class Automating_POST_Request {
 		// Applying validations on response body & Header
 		.body("scope", equalTo("APP"))
 		.header("Server", "Apache/2.4.52 (Ubuntu)");         
-		
-
 	}
 
 }
