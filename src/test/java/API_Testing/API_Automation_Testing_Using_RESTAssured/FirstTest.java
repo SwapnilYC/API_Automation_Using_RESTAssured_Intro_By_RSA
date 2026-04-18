27d2bb0d12b7536fcda536ad0fbdfc6055898000
Swapnil Chaudhari <swap95nil@gmail.com>
2026-04-18 18:07:33 +0530

    first commit

---
+++ src/test/java/API_Testing/API_Automation_Testing_Using_RESTAssured/FirstTest.java
@@ -0,0 +1,10 @@
+package API_Testing.API_Automation_Testing_Using_RESTAssured;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
