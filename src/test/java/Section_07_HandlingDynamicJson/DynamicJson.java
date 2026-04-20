856d8b59c256a6c5b6f48da9c9fc1275865a78de
Swapnil Chaudhari <swap95nil@gmail.com>
2026-04-20 16:41:00 +0530

    This libarry api can only delete the books added by simple numbers or given in api documentation/contract. So no worries. Here Dataprovider can be used to add multiple dataset. As i already have an idea baout dataprovider annotation so i have not integrated it here. But when data is coming from external jason file then i will write code for dataprovider

--- src/test/java/Section_07_HandlingDynamicJson/DynamicJson.java
+++ src/test/java/Section_07_HandlingDynamicJson/DynamicJson.java
@@ -10,28 +10,38 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 
 public class DynamicJson {
-	@Test
-	public void addBook() {
+
+	public String addBook() {
 		RestAssured.baseURI = "http://216.10.245.166";
 		String resource = "/Library/Addbook.php";
-		String bookToBeAdded = PayLoad.addBook("Snehal","Mumbai425413");
+		String bookToBeAdded = PayLoad.addBook("SnehalCH002","Mumbai425413");
 		String response = given().header("Content-Type","application/json").body(bookToBeAdded)
 		.when().post(resource)
 		.then().assertThat().statusCode(200).extract().response().asString();
 		
 		JsonPath JPObj = ReusableJsonPath.rawToJson(response);
 		System.out.println(JPObj.getString("Msg"));
-		System.out.println(JPObj.getString("ID"));
+		String ID = JPObj.getString("ID");
+		System.out.println(ID);
+		return ID;
+	}
+	
+	@Test
+	public void deleteBook() {
+		RestAssured.baseURI = "http://216.10.245.166";
+		String resource = "/Library/DeleteBook.php";
+//		String IDOfBookToBeDeleted = addBook();
+		String IDOfBookToBeDeleted = "bcd227";
+		System.out.println("ID Of Book To Be Deleted: " + IDOfBookToBeDeleted);
+		
+		String response = given().header("Content-Type","application/json").body("{\r\n"
+				+ "\"ID\" : \""+IDOfBookToBeDeleted+"\"}")
+		.when().post(resource)
+		.then().assertThat().statusCode(200).extract().response().asString();
+		
+		JsonPath JPObj = ReusableJsonPath.rawToJson(response);
+//		System.out.println(JPObj.getString("Msg"));
+//		String ID = JPObj.getString("ID");
+		System.out.println(JPObj.getString("msg"));
 	}
 }
-
-
-/*
-Practical Problems:
-	1. Dynamically build json payload with external data inputs
-		- Now body payload is hardcoded in code(payload class)
-	2. Parameterize the API Tests with multiple data sets
-		- Currently if we want to change data we have to manually change in payload class
-		- We cant do it on the fly
-	3. How to send static Json files(payload) directly into Post Method of Rest Assured
- */
